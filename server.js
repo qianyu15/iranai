3dabbdbefd1ad457f7ee4f3dd64a97b70dcc46f9
Qianyu Yang <qianyuyangvip@gmail.com>
2026-07-20 08:12:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const ex = require("express");
 const app = ex();
 app.get("/proxy",(q,r)=>{
   fetch(q.query.url||"https://example.com").then(r=>r.text()).then(data=>{
-  r.send(data)
+  r.send(data);res.setHeader("Access-Control-Allow-Origin","*");
 });
 });
 app.listen(8000,()=>console.log("やっとCORS回避できたわ"));
